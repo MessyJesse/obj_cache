6eb5595ed6f8f039e32f8199538fe80eeae4ebb2
Jesse DuMond <jesse.dumond@gmail.com>
2015-10-23 14:51:57 -0400

    Fix some whitespace issues

--- obj_cache.h
+++ obj_cache.h
@@ -3,7 +3,7 @@
 struct obj_cache;
 
 struct obj_cache *obj_cache_create(size_t size, size_t align); 
-void *obj_cache_alloc(struct obj_cache * cache); 
-void obj_cache_free(struct obj_cache *cache, void * obj);
+void *obj_cache_alloc(struct obj_cache *cache); 
+void obj_cache_free(struct obj_cache *cache, void *obj);
 void obj_cache_destroy(struct obj_cache *cache);
 
